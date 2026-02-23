a81fe3167dfddc99ed88b11ba2ef6802cb2b4040
Tanner Oleksiuk <tanneroleksiuk@gmail.com>
2026-02-22 22:12:45 -0800

    Add some keybindings for telescope

--- lua/tanner/lzplugins/telescope.lua
+++ lua/tanner/lzplugins/telescope.lua
@@ -1,9 +1,26 @@
 return {
     {
-        "nvim-telescope/telescope.nvim", version = "*",
+        "nvim-telescope/telescope.nvim", version = "0.2.1",
+
         dependencies = {
             "nvim-lua/plenary.nvim",
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        },
+        
+        -- Configure Key Mappings
+        keys = {
+            -- Keymapping for finding files
+            {
+                "<leader>ff",
+                function() require("telescope.builtin").find_files() end,
+                desc = "Find files",
+            },
+            -- Keymapping for live grep
+            {
+                "<leader>lg",
+               function() require("telescope.builtin").live_grep() end,
+                desc = "Live Grep",
+            },
         }
     }
 }
