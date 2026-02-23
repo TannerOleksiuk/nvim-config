4c37ff4da00d470eb5d81736d6b43ffbf3ae43f5
Tanner Oleksiuk <tanneroleksiuk@gmail.com>
2026-02-22 20:48:19 -0800

    Set colourscheme in init file

--- lua/tanner/init.lua
+++ lua/tanner/init.lua
@@ -2,3 +2,7 @@ require("tanner.remap")
 require("tanner.options")
 -- Lazy plugin manager should load after mappings/options have been set
 require("tanner.lazy")
+
+-- Set colours/theme
+vim.cmd.colorscheme('tokyonight')
+
